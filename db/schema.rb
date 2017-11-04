9a1418dc25f0077b5307e08d82b1508ec1207f7e
smarzz <c.v.marzari@gmail.com>
2017-11-04 19:33:51 +0000

    adding usr id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103054226) do
+ActiveRecord::Schema.define(version: 20171104190628) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "post"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
